460d46585695840c6fc5f052919f2f98e82cdcf0
Yerassyliitu <34080@iitu.edu.kz>
2024-02-16 14:56:49 +0600

    allowed_hosts = *

--- nic_site/settings.py
+++ nic_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7!pp30dnp29ezeh=34!rd$68_a_1qn&w)j68$7bfgjttxeqi1h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
